7fe2d7e10b083f31e8929f43c66e1c3c3a1a4b02
libang <libang@giga.build>
2016-06-05 01:49:54 +0800

    mdified chat

--- chat/server.js
+++ chat/server.js
@@ -4,9 +4,13 @@ var users = {};
 
 var server = net.createServer(function (conn) {
   var nickName;
+  var names = Object.keys(users).map(function (name) {
+    return name;
+  }).join(',');
   conn.setEncoding('utf8');
   conn.write('欢迎来到聊天室\n');
-  conn.write('共有' + count + '位其他用户在线\n');
+  conn.write('共有' + count + '位用户在线\n');
+  if (names) conn.write(names + '\n');
   conn.write('请输入你的用户名,回车确认:');
   count++;
 
@@ -14,14 +18,16 @@ var server = net.createServer(function (conn) {
     count--;
     delete users[nickName];
     broadcast(nickName + '离开了聊天室\n');
+    broadcast('还有' + count + '位用户在线\n');
   });
 
   conn.on('data', function (data) {
     data = data.replace('\r\n', '');
     if (!nickName) {
+      if (users[data]) return conn.write('你输入的用户名已存在请重新输入:');
       nickName = data;
       users[nickName] = conn;
-      broadcast(nickName + '加入聊天室\n');
+      broadcast(nickName + '加入聊天室\n', true);
       return;
     }
     broadcast(nickName + '说:' + data + '\n', true);
@@ -34,6 +40,6 @@ var server = net.createServer(function (conn) {
   }
 });
 
-server.listen(3000, function () {
+server.listen(3000, '0,0,0,0', function () {
   console.log('server listen on :3000');
 });
